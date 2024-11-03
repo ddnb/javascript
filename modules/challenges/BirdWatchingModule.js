3c78b62bdae38886212315c884f9a535076636f7
lehungio <me@lehungio.com>
2024-11-04 01:14:46 +0900

    Challenge - Bird Watching

---
+++ modules/challenges/BirdWatchingModule.js
@@ -0,0 +1,47 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+  return birdsPerDay.reduce((result, dayCount) => result + dayCount, 0);
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+  return birdsPerDay
+    .slice((week - 1) * 7, week * 7)
+    .reduce((result, dayCount) => result + dayCount, 0);
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+// export function fixBirdCountLog(birdsPerDay) {
+//   return birdsPerDay.map((day, index) => (index % 2 === 0 ? day + 1 : day));
+// }
+export function fixBirdCountLog(birdsPerDay) {
+  for (let i = 0; i < birdsPerDay.length; i += 2) {
+    birdsPerDay[i] += 1;
+  }
+  return birdsPerDay;
+}
+
+// TODO birdCountLog - Create a function that returns the bird count log new array
